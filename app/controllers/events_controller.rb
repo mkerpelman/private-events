151b73d8aa9a540a19bd245f3ef1471dbad1ed56
Mike Work <michael.kerpelman@gmail.com>
2021-06-04 11:09:46 -0400

    Create event, show event details w/ link to creator page, show user page w/ organized events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,27 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :date, :time)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   root "events#index"
-  devise_for :users, :controllers => { registrations: 'registrations' } 
-  resources :events, only: [:index]
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :users, only: [:show] 
+  resources :events, only: [:index, :new, :create, :show]
 
 end
