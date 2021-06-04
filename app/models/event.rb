f393597ef9d5f8ce8e772b6c92d27e1b895d4e21
Mike Work <michael.kerpelman@gmail.com>
2021-06-04 12:48:37 -0400

    Create associations between attendees and events, no migration

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,8 @@ class Event < ApplicationRecord
   validates :location, presence: true, length: {minimum: 3 }
 
   belongs_to :creator, class_name: "User"
+  has_many :attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :attendances
+
 
 end
